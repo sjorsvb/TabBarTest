c843d5a375616c842307c65c00d680db22ad06e7
Sjors van Berkel <sjorsvanberkel@gmail.com>
2017-10-20 10:18:12 +0200

    Initial Commit

---
+++ TabBarTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabBarTest
+//
+//  Created by Sjors van Berkel on 20-10-17.
+//  Copyright © 2017 Sjors van Berkel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
